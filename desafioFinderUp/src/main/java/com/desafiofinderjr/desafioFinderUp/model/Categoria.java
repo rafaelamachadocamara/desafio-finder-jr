5a9e3140796e4625543c6f8372bfc98db7ea02fb
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-14 17:03:44 -0300

    model entity

---
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/model/Categoria.java
@@ -0,0 +1,35 @@
+package com.desafiofinderjr.desafioFinderUp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
