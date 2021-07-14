f78c72321f9421540df967acee3e0dc41e0e4ab3
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-14 16:41:39 -0300

    configurando o projeto

---
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/DesafioFinderUpApplication.java
@@ -0,0 +1,13 @@
+package com.desafiofinderjr.desafioFinderUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinderUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFinderUpApplication.class, args);
+	}
+
+}
---
+++ desafioFinderUp/src/test/java/com/desafiofinderjr/desafioFinderUp/DesafioFinderUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiofinderjr.desafioFinderUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinderUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
