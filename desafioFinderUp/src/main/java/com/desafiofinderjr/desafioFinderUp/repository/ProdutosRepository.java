16c1bd9bf2afff215bd4ca637b374a6cc847480d
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-14 17:16:41 -0300

    adicionando anotações no repository de produtos

--- desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/repository/ProdutosRepository.java
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/repository/ProdutosRepository.java
@@ -1,5 +1,11 @@
 package com.desafiofinderjr.desafioFinderUp.repository;
 
-public interface ProdutosRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafiofinderjr.desafioFinderUp.model.Produtos;
+
+@Repository
+public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
 
 }
