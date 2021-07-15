f3eac96be293914004caec8f74675518440ef5d5
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-15 13:08:36 -0300

    configurando model de categoria

--- desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/model/Categoria.java
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/model/Categoria.java
@@ -1,10 +1,16 @@
 package com.desafiofinderjr.desafioFinderUp.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -15,6 +21,10 @@ public class Categoria {
 	private long id;
 	
 	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produtos> produto;
 
 	public long getId() {
 		return id;
