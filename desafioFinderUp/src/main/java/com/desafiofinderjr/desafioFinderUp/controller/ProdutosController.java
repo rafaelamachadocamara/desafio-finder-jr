eb2cf9bd2d31e276f0bc5b6aca1e7f922a17d4c6
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-15 13:56:27 -0300

    metodo Put

--- desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/ProdutosController.java
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/ProdutosController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,11 @@ public class ProdutosController {
 	@PostMapping
 	public ResponseEntity<Produtos> post(@RequestBody Produtos produtos) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produtos));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produtos> put(@RequestBody Produtos produtos) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produtos));
 	}
 }
 
