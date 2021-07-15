d9ce8fde0d8556f5d09cd747140dee58d07ddf2b
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-15 14:04:24 -0300

    metodo delete

--- desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/CategoriaController.java
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +48,9 @@ public class CategoriaController {
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(repository.save(categoria));
 }
-	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}		
+}
