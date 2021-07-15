390b4a157582b64573fb6a7f0637238fdcac58d4
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-15 13:16:56 -0300

    metodo FindById

--- desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/CategoriaController.java
+++ desafioFinderUp/src/main/java/com/desafiofinderjr/desafioFinderUp/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class CategoriaController {
 	@GetMapping
 	public ResponseEntity<List<Categoria>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
